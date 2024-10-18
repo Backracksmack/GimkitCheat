32b82f4ed49cecb9ad387c791a8a498e80bc2200
SchoolCheats <grahameast673@gmail.com>
2024-10-18 14:37:54 -0500

    Delete src directory

--- src/scripts/knockback/Knockback.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Group from "../../hud/Group.svelte";
-    import Rapidfire from "../shared/Rapidfire.svelte";
-</script>
-
-<Group name="Knockback">
-    <Rapidfire message="Rapid fire" hotkeyId="knockbackRapidFire" />
-</Group>
